e9bd96d44acd25c0b10a70855b51766f6973d5c6
Anugrah Singhal <anugrahsinghal1@gmail.com>
2023-05-16 16:50:15 +0530

    git push stage-2
    send os.stdout/err to command
    
    run the command
    
    exit if error

--- app/main.go
+++ app/main.go
@@ -14,19 +14,19 @@ func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	//Uncomment this block to pass the first stage!
-
 	command := os.Args[3]
 	args := os.Args[4:len(os.Args)]
 
 	cmd := exec.Command(command, args...)
-	output, err := cmd.Output()
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err := cmd.Run()
+
 	if err != nil {
 		fmt.Printf("Err: %v", err)
 		os.Exit(1)
 	}
-
-	fmt.Println(string(output))
 
 	// Your task is to implement a very basic version of docker run. It will be executed similar to docker run:
 	//
